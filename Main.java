98eee30103867bef5b096e4408eb5044f35f4924
DTC-CS-Camp <68837059+DTC-CS-Camp@users.noreply.github.com>
2021-08-01 09:55:45 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,47 +1,12 @@
 class Main {
   public static void main(String[] args) {
-		/*PROBLEM 1
-		 *test cases for Problem 1.
-		 *when you're ready, uncomment the line below (with a regular comment),
-		 *and then click the play button.
-		**/
-		//Problem1.main(args);
-
-		/*PROBLEM 2
-		 *test cases for Problem 2.
-		 *when you're ready, uncomment the line below (with a regular comment),
-		 *and then click the play button.
-		**/
-	  
-    //Uncomment line below to test getGrade()
-		//System.out.println("This student got an " + Student.getGrade() + " grade");
-
-    //Uncomment line below to test getAge()
-		//System.out.println("This student is " + Student.getAge() + " years old");
-
-    //Uncomment the lines below to test getGender()
-		//System.out.println("This student currently has a gender value of " + Student.getGender());
-
-    //Uncomment line below to test getMoney()
-		//System.out.println("This student had " + Student.getMoney() + " dollars");
-
-    //Uncomment the lines below to test changeGender()
-		//System.out.println("This student currently has a gender value of " + Student.getGender());
-    //Student.changeGender();
-    //System.out.println("This student now has a gender value of " + Student.getGender());
-
-    //NOTE: THIS SHOULD ERROR - IT SHOULD NOT RUN
-    //IF IT RUNS, CHECK WITH YOUR MENTOR
-		//Uncomment line below to test giveMoney()
-		//System.out.println("This student had " + //Student.getMoney() + " dollars");
-    //Student.giveMoney(10); //this line should error
-    //System.out.println("This student now has " + //Student.getMoney() + " dollars");
-    //Student.giveMoney(-10); //this line should error
-
-    //Uncomment line below to test haveBirthday()
-		//System.out.println("This student has " + //Student.getMoney() + " dollars");
-    //Student.haveBirthday();
-    //System.out.println("This student now has " + //Student.getMoney() + " dollars, since they have a grade of " + Student.getGrade());
+		/*
+    Create your Student objects and use the methods here in main!
+    
+    Remember the syntax of how to make an object and be mindful of matching your arguments to the order of parameters in your constructor.
+  
+    Student objectName= new Student( arguments );
+    */
 
 
 	}
