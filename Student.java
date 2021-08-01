12377088fb565bfdeffa563ea7920081a00b23c2
DTC-CS-Camp <68837059+DTC-CS-Camp@users.noreply.github.com>
2021-08-01 09:57:12 -0600

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,14 @@
-//Write your class for Problem 2 in here
-//Make sure you have correct syntax for each
-//your methods, especially spelling, since otherwise
-//when you run the program it will fail.
-
+/*Write your Student class below!
+  We've created the class body and added the first private instance variable for you.
+  Make sure your spelling and syntax are correct! Be careful with your curly brackets { }
+*/
 //EDIT CODE BELOW \/
+public class Student{
+  private int age;
 
+
+  
+
+
+}
 //EDIT CODE ABOVE /\
